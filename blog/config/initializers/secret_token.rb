24767017f9150ebf0861f4b289e7519b8d357f55
caneroj1 <caneroj1@tcnj.edu>
2014-04-11 20:15:24 -0400

    learing ruby on rails. making a rudimentary blogging system that uses twitter bootstrap for styling.

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'a329fe95332196a92d493cf091e669f30b42055c8518b126da73ebeab48ec52a6d48e787cb0ff42a279d5b2af704323f20fb26613c7270b993823cc01743192b'
